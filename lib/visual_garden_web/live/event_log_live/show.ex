4a9cdf0f7e370da8868ef2fbccfbd8a84c1bcd05
Theron Boerner <hunterboerner@gmail.com>
2024-04-09 17:52:54 -0400

    events

--- lib/visual_garden_web/live/event_log_live/show.ex
+++ lib/visual_garden_web/live/event_log_live/show.ex
@@ -18,4 +18,31 @@ defmodule VisualGardenWeb.EventLogLive.Show do
 
   defp page_title(:show), do: "Show Event log"
   defp page_title(:edit), do: "Edit Event log"
+
+  def render_events(assigns) do
+    ~H"""
+    <div class="flow-root">
+      <ul role="list" class="-mb-8" id="event-log-list" phx-update="stream">
+        <%= for {_name, event} <- @events do %>
+          <li title="Event type" id={"event_log_#{event.id}"}>
+            <div class="relative pb-8">
+              <span class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true">
+              </span>
+            </div>
+            <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+              <div>
+                <p class="text-sm text-gray-500">
+                  <%= event.event_type %>
+                </p>
+              </div>
+              <div class="whitespace-nowrap text-right text-sm text-gray-500">
+                <time datetime="2020-09-20">Sep 20</time>
+              </div>
+            </div>
+          </li>
+        <% end %>
+      </ul>
+    </div>
+    """
+  end
 end
