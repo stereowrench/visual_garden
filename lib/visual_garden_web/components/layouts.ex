ca3dd20ce0702fce8c9c6296ff298695d7f019bf
Theron Boerner <hunterboerner@gmail.com>
2024-05-14 18:45:28 -0400

    Styling

--- lib/visual_garden_web/components/layouts.ex
+++ lib/visual_garden_web/components/layouts.ex
@@ -7,12 +7,17 @@ defmodule VisualGardenWeb.Layouts do
   attr :name, :atom, required: true
   attr :path, :string, required: true
   attr :text, :string, required: true
+  attr :icon, :string, default: nil
+
   def nav_link(%{} = assigns) do
     ~H"""
     <.link
       navigate={@path}
       class={(if @active == @name, do: "bg-gray-50 text-indigo-600", else: "text-gray-700 hover:text-indigo-600 hover:bg-gray-50") <> " group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"}
     >
+      <%= if @icon do %>
+        <.icon name={@icon} />
+      <% end %>
       <%= @text %>
     </.link>
     """
