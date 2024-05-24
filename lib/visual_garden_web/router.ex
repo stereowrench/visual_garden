39db621a4126c5c4386f771489efaed4bf5b5552
Theron Boerner <hunterboerner@gmail.com>
2024-05-24 17:17:17 -0400

    Generate user auth w/ minor changes

--- lib/visual_garden_web/router.ex
+++ lib/visual_garden_web/router.ex
@@ -1,5 +1,7 @@
 defmodule VisualGardenWeb.Router do
   use VisualGardenWeb, :router
+
+  import VisualGardenWeb.UserAuth
   import PhoenixStorybook.Router
 
   pipeline :browser do
@@ -9,6 +11,7 @@ defmodule VisualGardenWeb.Router do
     plug :put_root_layout, html: {VisualGardenWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -161,6 +164,56 @@ defmodule VisualGardenWeb.Router do
 
       live_dashboard "/dashboard", metrics: VisualGardenWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", VisualGardenWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [
+        {VisualGardenWeb.UserAuth, :redirect_if_user_is_authenticated},
+        VisualGardenWeb.Nav,
+        Flashy.Hook
+      ] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", VisualGardenWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [
+        {VisualGardenWeb.UserAuth, :ensure_authenticated},
+        VisualGardenWeb.Nav,
+        Flashy.Hook
+      ] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", VisualGardenWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [
+        {VisualGardenWeb.UserAuth, :mount_current_user},
+        VisualGardenWeb.Nav,
+        Flashy.Hook
+      ] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
