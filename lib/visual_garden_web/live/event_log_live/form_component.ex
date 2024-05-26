5999c863cf9327a1c365c4db7411f51bd6c83156
Theron Boerner <hunterboerner@gmail.com>
2024-05-26 10:38:23 -0400

    Use live select for product transfer

--- lib/visual_garden_web/live/event_log_live/form_component.ex
+++ lib/visual_garden_web/live/event_log_live/form_component.ex
@@ -1,4 +1,5 @@
 defmodule VisualGardenWeb.EventLogLive.FormComponent do
+  alias VisualGardenWeb.KeywordHighlighter
   alias VisualGarden.Authorization.UnauthorizedError
   alias VisualGarden.Authorization
   alias VisualGarden.Gardens.EventLog
@@ -41,18 +42,17 @@ defmodule VisualGardenWeb.EventLogLive.FormComponent do
           <.input field={@form[:till_depth_in]} type="number" label="Till depth in" step="any" />
         <% end %>
         <%= if @action == :transfer do %>
-          <.input
+          <.live_select
             field={@form[:transferred_from_id]}
-            type="text"
-            label="Transferred From"
-            list="product-list"
-          />
-
-          <datalist id="product-list">
-            <%= for product <- @products do %>
-              <option value={product.id}><%= product.name %></option>
-            <% end %>
-          </datalist>
+            value_mapper={&to_string(&1)}
+            label="Transferred from"
+            phx-target={@myself}
+            options={@product_opts}
+          >
+            <:option :let={opt}>
+              <.highlight matches={opt.matches} string={opt.label} />
+            </:option>
+          </.live_select>
 
           <.input
             field={@form[:transferred_amount]}
@@ -79,6 +79,10 @@ defmodule VisualGardenWeb.EventLogLive.FormComponent do
     """
   end
 
+  def highlight(assigns) do
+    KeywordHighlighter.highlight(assigns.string, assigns.matches)
+  end
+
   defp event_type_for_action(:new_water), do: "water"
   defp event_type_for_action(:till), do: "till"
   defp event_type_for_action(:transfer), do: "transfer"
@@ -90,11 +94,44 @@ defmodule VisualGardenWeb.EventLogLive.FormComponent do
     changeset =
       Gardens.change_event_log(el, %{"event_type" => event_type_for_action(assigns.action)})
 
+    product_opts =
+      Enum.map(assigns.products, fn p ->
+        %{label: p.name, value: to_string(p.id), matches: []}
+      end)
+
     {:ok,
      socket
      |> assign(assigns)
+     |> assign(
+       :product_opts,
+       product_opts
+     )
+     |> assign(
+       :product_opts_all,
+       product_opts
+     )
      |> assign(:event_log, el)
      |> assign_form(changeset)}
+  end
+
+  @impl true
+  def handle_event(
+        "live_select_change",
+        %{"text" => text, "id" => live_select_id = _},
+        socket
+      ) do
+    matches =
+      Seqfuzz.matches(socket.assigns.product_opts_all, text, & &1.label, filter: true, sort: true)
+
+    opts =
+      Enum.map(matches, fn {map, c} ->
+        %{label: map.label, value: map.value, matches: c.matches}
+      end)
+      |> Enum.take(10)
+
+    send_update(LiveSelect.Component, id: live_select_id, options: opts)
+
+    {:noreply, socket}
   end
 
   defp merge_attrs(params, action, product) do
