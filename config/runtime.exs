4cf080ca4544be10442b60ad5c2f8ca345b9b6e2
Theron Boerner <hunterboerner@gmail.com>
2024-05-26 18:22:22 -0400

    mail settings

--- config/runtime.exs
+++ config/runtime.exs
@@ -107,8 +107,12 @@ if config_env() == :prod do
   # Also, you may need to configure the Swoosh API client of your choice if you
   # are not using SMTP. Here is an example of the configuration:
   #
-  #     config :visual_garden, VisualGarden.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
+  config :visual_garden, VisualGarden.Mailer,
+    adapter: Swoosh.Adapters.Sendgrid,
+    api_key: System.get_env("SENDGRID_API_KEY")
+
+  config :visual_garden, :mail_from, System.get_env("MAIL_FROM")
+
   #       api_key: System.get_env("MAILGUN_API_KEY"),
   #       domain: System.get_env("MAILGUN_DOMAIN")
   #
