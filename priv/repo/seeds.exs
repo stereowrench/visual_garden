f98e88c15e8e1fd9cbdd01bbb42d30cf7af96f1d
Theron Boerner <hunterboerner@gmail.com>
2024-05-29 17:29:56 -0400

    Update seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,42 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias VisualGarden.Repo
+alias VisualGarden.Library
+alias VisualGarden.Library.{LibrarySeed, Schedule, Species, Region}
+import Ecto.Query
+
+Path.join([__DIR__, "./seed_data/Species.csv"])
+|> File.stream!()
+|> CSV.decode!(headers: true)
+|> Stream.map(fn
+  %{
+    "UUID" => uuid,
+    "Common Name" => name,
+    "Genus" => genus,
+    "Species" => species,
+    "Variety" => variety,
+    "Season" => season
+  } ->
+    case Repo.one(from sp in Species, where: sp.uuid == ^uuid) do
+      nil ->
+        {:ok, _} =
+          Library.create_species(%{
+            name: species,
+            genus: genus,
+            variant: variety,
+            season: season,
+            uuid: uuid,
+            common_name: name
+          })
+
+      sp ->
+        Species.changeset(
+          sp,
+          %{name: species, genus: genus, variant: variety, season: season, common_name: name}
+        )
+        |> Repo.update!()
+    end
+end)
+|> Stream.run()
