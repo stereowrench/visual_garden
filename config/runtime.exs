25c0feb020a51a4c7b1d1c598d91a2b008bcf33a
Theron Boerner <hunterboerner@gmail.com>
2024-06-02 15:22:02 -0400

    hope this doesn't break SSL

--- config/runtime.exs
+++ config/runtime.exs
@@ -67,8 +67,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base,
-    force_ssl: [hsts: true]
+    secret_key_base: secret_key_base
 
   # ## SSL Support
   #
