8be629758d65fead926df237dcf5f6502436e630
Theron Boerner <hunterboerner@gmail.com>
2024-06-02 12:17:19 -0400

    list garden name on task list

--- lib/visual_garden_web/live/home_live/show.ex
+++ lib/visual_garden_web/live/home_live/show.ex
@@ -139,7 +139,7 @@ defmodule VisualGardenWeb.HomeLive.Show do
       <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
         <div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
           <h3 class="text-base font-semibold leading-6 text-gray-900">
-            Plant <%= @entry.common_name %>
+            Plant <%= @entry.common_name %> in <%= @garden.name %>
           </h3>
         </div>
         <dl>
