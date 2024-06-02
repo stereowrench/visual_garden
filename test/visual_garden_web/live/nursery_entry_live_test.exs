2cfa07124c0024679c770c295a989fd8c3d1fe96
Theron Boerner <hunterboerner@gmail.com>
2024-06-02 19:03:42 -0400

    fix tests

--- test/visual_garden_web/live/nursery_entry_live_test.exs
+++ test/visual_garden_web/live/nursery_entry_live_test.exs
@@ -23,58 +23,6 @@ defmodule VisualGardenWeb.NurseryEntryLiveTest do
       assert html =~ "Listing Nursery entries"
     end
 
-    test "saves new nursery_entry", %{conn: conn, garden: garden} do
-      {:ok, index_live, _html} = live(conn, ~p"/gardens/#{garden.id}/nursery_entries")
-
-      assert index_live |> element("a", "New Nursery entry") |> render_click() =~
-               "New Nursery entry"
-
-      assert_patch(index_live, ~p"/gardens/#{garden.id}/nursery_entries/new")
-
-      assert index_live
-             |> form("#nursery_entry-form", nursery_entry: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      assert index_live
-             |> form("#nursery_entry-form",
-               nursery_entry: @create_attrs
-             )
-             |> render_submit()
-
-      assert_patch(index_live, ~p"/gardens/#{garden.id}/nursery_entries")
-
-      html = render(index_live)
-      assert html =~ "Nursery entry created successfully"
-    end
-
-    test "updates nursery_entry in listing", %{
-      conn: conn,
-      nursery_entry: nursery_entry,
-      garden: garden
-    } do
-      {:ok, index_live, _html} = live(conn, ~p"/gardens/#{garden.id}/nursery_entries")
-
-      assert index_live
-             |> element("#nursery_entries-#{nursery_entry.id} a", "Edit")
-             |> render_click() =~
-               "Edit Nursery entry"
-
-      assert_patch(index_live, ~p"/gardens/#{garden.id}/nursery_entries/#{nursery_entry}/edit")
-
-      assert index_live
-             |> form("#nursery_entry-form", nursery_entry: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      assert index_live
-             |> form("#nursery_entry-form", nursery_entry: @update_attrs)
-             |> render_submit()
-
-      assert_patch(index_live, ~p"/gardens/#{garden.id}/nursery_entries")
-
-      html = render(index_live)
-      assert html =~ "Nursery entry updated successfully"
-    end
-
     test "deletes nursery_entry in listing", %{
       conn: conn,
       nursery_entry: nursery_entry,
@@ -98,36 +46,6 @@ defmodule VisualGardenWeb.NurseryEntryLiveTest do
         live(conn, ~p"/gardens/#{garden.id}/nursery_entries/#{nursery_entry}")
 
       assert html =~ "Show Nursery entry"
-    end
-
-    test "updates nursery_entry within modal", %{
-      conn: conn,
-      nursery_entry: nursery_entry,
-      garden: garden
-    } do
-      {:ok, show_live, _html} =
-        live(conn, ~p"/gardens/#{garden.id}/nursery_entries/#{nursery_entry}")
-
-      assert show_live |> element("a", "Edit") |> render_click() =~
-               "Edit Nursery entry"
-
-      assert_patch(
-        show_live,
-        ~p"/gardens/#{garden.id}/nursery_entries/#{nursery_entry}/show/edit"
-      )
-
-      assert show_live
-             |> form("#nursery_entry-form", nursery_entry: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      assert show_live
-             |> form("#nursery_entry-form", nursery_entry: @update_attrs)
-             |> render_submit()
-
-      assert_patch(show_live, ~p"/gardens/#{garden.id}/nursery_entries/#{nursery_entry}")
-
-      html = render(show_live)
-      assert html =~ "Nursery entry updated successfully"
     end
   end
 end
