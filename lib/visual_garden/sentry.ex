700cd88690a0be41d17b7907328759044621e806
Theron Boerner <hunterboerner@gmail.com>
2024-06-09 11:10:23 -0400

    filter out some errors

--- lib/visual_garden/sentry.ex
+++ lib/visual_garden/sentry.ex
@@ -1,6 +1,12 @@
 defmodule VisualGarden.Sentry do
   def filter_non_500(%Sentry.Event{original_exception: exception} = event) do
     cond do
+      exception == Bandit.HTTPError ->
+        false
+
+      %{message: "Timeout waiting for space in the send_window"} = exception ->
+        false
+
       Plug.Exception.status(exception) < 500 ->
         false
 
