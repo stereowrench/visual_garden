b3a633c617ea96aa416045ccb6998d88af1c00dd
Theron Boerner <hunterboerner@gmail.com>
2024-06-09 11:14:34 -0400

    pattern match error

--- lib/visual_garden/sentry.ex
+++ lib/visual_garden/sentry.ex
@@ -1,7 +1,7 @@
 defmodule VisualGarden.Sentry do
   def filter_non_500(%Sentry.Event{original_exception: exception} = event) do
     cond do
-      exception == Bandit.HTTPError ->
+      %Bandit.HTTPError{} = exception ->
         false
 
       %{message: "Timeout waiting for space in the send_window"} = exception ->
