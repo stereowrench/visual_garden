5c00a475dc9b4d54851cd22a6e23a2398187f231
Theron Boerner <hunterboerner@gmail.com>
2024-06-12 18:51:01 -0400

    add SASL logging

--- config/config.exs
+++ config/config.exs
@@ -58,6 +58,9 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :logger,
+  handle_sasl_reports: true
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
