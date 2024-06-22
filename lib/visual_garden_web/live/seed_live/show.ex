b5bbb5f1940b76d37c9a22d893a932d14bcc4901
Theron Boerner <hunterboerner@gmail.com>
2024-06-22 16:42:15 -0400

    fix up rendering for seed show

--- lib/visual_garden_web/live/seed_live/show.ex
+++ lib/visual_garden_web/live/seed_live/show.ex
@@ -1,4 +1,6 @@
 defmodule VisualGardenWeb.SeedLive.Show do
+  alias VisualGarden.Repo
+  alias VisualGarden.Library
   use VisualGardenWeb, :live_view
 
   alias VisualGarden.Gardens
@@ -17,12 +19,20 @@ defmodule VisualGardenWeb.SeedLive.Show do
      |> assign(:garden, garden)}
   end
 
+  defp render_species(species, common_name) do
+    species_display_string(species, common_name)
+  end
+
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
+    common_names = Library.list_species_with_common_names() |> Enum.into(%{})
+    seed = Gardens.get_seed!(id) |> Repo.preload(:harvest_species)
+
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:seed, Gardens.get_seed!(id))}
+     |> assign(:common_name, common_names[seed.species])
+     |> assign(:seed, seed)}
   end
 
   defp page_title(:show), do: "Show Plantable"
