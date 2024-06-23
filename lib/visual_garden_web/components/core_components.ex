e420adc0cc6f18a288bb01582f5e529c04d81ad6
Theron Boerner <hunterboerner@gmail.com>
2024-06-23 15:27:56 -0400

    sticky nav

--- lib/visual_garden_web/components/core_components.ex
+++ lib/visual_garden_web/components/core_components.ex
@@ -568,7 +568,7 @@ defmodule VisualGardenWeb.CoreComponents do
 
   def back(assigns) do
     ~H"""
-    <div class="mt-16">
+    <div class="sticky top-0 bg-white">
       <.link
         navigate={@navigate}
         class="text-sm font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
@@ -592,7 +592,7 @@ defmodule VisualGardenWeb.CoreComponents do
 
   def forward(assigns) do
     ~H"""
-    <div>
+    <div class="sticky top-0 bg-white">
       <.link
         navigate={@navigate}
         class="float-right text-sm font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
@@ -612,7 +612,7 @@ defmodule VisualGardenWeb.CoreComponents do
 
   def forward_back(assigns) do
     ~H"""
-    <div>
+    <div class="sticky top-0 bg-white">
       <.link
         navigate={@navigate_backward}
         class="text-sm font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
