495f61c75f843dd3510aa0f0388cfca6ff52cbdb
Theron Boerner <hunterboerner@gmail.com>
2024-07-27 13:12:12 -0400

    work on scraping

--- data/seedsnow.py
+++ data/seedsnow.py
@@ -1,6 +1,8 @@
 import scrapy
 import string
 import logging
+from urllib.parse import urljoin
+from scrapy.utils.response import open_in_browser
 
 class BlogSpider(scrapy.Spider):
     name = 'seedsnow-spider'
@@ -10,10 +12,22 @@ class BlogSpider(scrapy.Spider):
     for letter in list("t"):
         start_urls.append('https://www.seedsnow.com/collections/alphabetical-begins-with-' + letter)
 
+    def parse_item(self, response):
+        # open_in_browser(response)
+        category = response.css('.breadcrumbs-list > :nth-child(2) > a::text').get().strip()
+        title = response.css('.product-title::text').get().strip()
+        description = " ".join(response.css(".disclosure__content.rte ::text").getall()).strip()
+        yield {"category": category, "title": title, "description": description, "url": response.url}
+
     def parse(self, response):
-        for title in response.css('.product-item__title.text--strong.link'):
-            yield {'title': title.css('::text').get()}
+        # url = urljoin(response.url, "/collections/shop-tomato-seeds/products/tomato-abraham-abe-lincoln")
+        # yield response.follow(url, self.parse_item)
+        for title in response.css('.js-pagination-result .card__title > .card-link'):
+            # yield {'title': title.css('::text').get()}
+            url = urljoin(response.url, title.css("::attr(href)").get())
+            yield response.follow(url, self.parse_item)
         # for next_page in response.css('alphabet'):
         #     yield response.follow(next_page, self.parse)
-        for next_page in response.css('.pagination__next.link'):
+        for next_page in response.css('.pagination--modern > a'):
+            print(next_page)
             yield response.follow(next_page, self.parse)
---
+++ data/seedsnow_to_gemini.py
@@ -0,0 +1,44 @@
+"""
+Install the Google AI Python SDK
+
+$ pip install google-generativeai
+
+See the getting started guide for more information:
+https://ai.google.dev/gemini-api/docs/get-started/python
+"""
+
+import os
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+# Create the model
+# See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+chat_session = model.start_chat(
+  history=[
+  ]
+)
+
+product = """
+
+"""
+
+response = chat_session.send_message("Extract the days to maturity in JSON format and summarize the description of the product in 3-5 keywords.\n" + product)
+
+print(response.text)
